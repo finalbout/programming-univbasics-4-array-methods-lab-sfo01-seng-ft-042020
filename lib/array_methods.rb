a5f743d920ebfe26ffbff71fec48e32df80a2a43
John Tom Jr <johntjr.22@gmail.com>
2020-03-06 00:23:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,15 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+sort = ["lame","I","wow"]
+sort.sort
 end
 
 def using_reverse(array)
-
+aim = ["wow","arrays!"]
+aim.reverse
 end
 
 def using_first(array)
